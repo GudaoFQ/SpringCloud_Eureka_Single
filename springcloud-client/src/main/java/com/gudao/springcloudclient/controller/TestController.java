c5406fc6e11eadda1fa4b444c42bf5b9a46da48c
zhang <2873908996@qq.com>
2020-12-25 10:16:25 +0800

    SpringCloud-Eureka单机搭建

---
+++ springcloud-client/src/main/java/com/gudao/springcloudclient/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.gudao.springcloudclient.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Author : GuDao
+ * 2020-12-24
+ */
+@RestController
+@RequestMapping("/api/v1/test")
+public class TestController {
+
+}
